353b89cdea79b54f5b366bb93e0a1fbd1eb9e0cc
StarHeartHunt <starheart233@gmail.com>
2022-03-16 20:42:48 +0800

    :fire: remove unused config

--- nonebot/adapters/feishu/config.py
+++ nonebot/adapters/feishu/config.py
@@ -13,7 +13,6 @@ class Config(BaseModel):
       - ``feishu_app_secret``: 飞书开放平台后台“凭证与基础信息”处给出的 App Secret
       - ``feishu_encrypt_key``: 飞书开放平台后台“事件订阅”处设置的 Encrypt Key
       - ``feishu_verification_token``: 飞书开放平台后台“事件订阅”处设置的 Verification Token
-      - ``feishu_tenant_access_token``: 请求飞书 API 后返回的租户密钥
       - ``feishu_is_lark``: 是否使用 Lark（飞书海外版），默认为 false
 
     """
@@ -22,7 +21,6 @@ class Config(BaseModel):
     feishu_app_secret: Optional[str] = Field(default=None)
     feishu_encrypt_key: Optional[str] = Field(default=None)
     feishu_verification_token: Optional[str] = Field(default=None)
-    feishu_tenant_access_token: Optional[str] = Field(default=None)
     feishu_is_lark: Optional[str] = Field(default=False)
 
     class Config:
