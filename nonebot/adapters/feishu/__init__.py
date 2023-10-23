403b2044bf0f08b7f09736eb1c46c45d116793ec
StarHeartHunt <starheart233@gmail.com>
2023-10-23 12:34:55 +0800

    :children_crossing: export post elements

--- nonebot/adapters/feishu/__init__.py
+++ nonebot/adapters/feishu/__init__.py
@@ -3,10 +3,18 @@
 from .bot import Bot as Bot
 from .utils import log as log
 from .event import *  # noqa: F403
+from .message import PostA as PostA
+from .message import PostAt as PostAt
 from .adapter import Adapter as Adapter
 from .message import Message as Message
+from .message import PostImg as PostImg
+from .message import PostText as PostText
+from .message import PostMedia as PostMedia
+from .message import PostEmotion as PostEmotion
 from .exception import ActionFailed as ActionFailed
 from .exception import NetworkError as NetworkError
 from .message import MessageSegment as MessageSegment
+from .message import PostMessageNode as PostMessageNode
 from .exception import ApiNotAvailable as ApiNotAvailable
 from .exception import FeishuAdapterException as FeishuAdapterException
+from .message import PostMessageNodeStylable as PostMessageNodeStylable
