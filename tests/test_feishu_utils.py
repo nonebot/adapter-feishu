976e89fc8bff2d1a084d2effa1bfac8493c3c20b
StarHeartHunt <starheart233@gmail.com>
2024-02-20 10:36:14 +0800

    :white_check_mark: add matrix for bytes key

--- tests/test_feishu_utils.py
+++ tests/test_feishu_utils.py
@@ -1,8 +1,11 @@
+import pytest
+
 from nonebot.adapters.feishu.utils import AESCipher
 
 
-def test_aes():
+@pytest.mark.parametrize("key", ["test key", b"test key"])
+def test_aes(key: str):
     encrypt = "P37w+VZImNgPEO1RBhJ6RtKl7n6zymIbEG1pReEzghk="
-    cipher = AESCipher("test key")
+    cipher = AESCipher(key)
 
     assert cipher.decrypt_string(encrypt) == "hello world"
