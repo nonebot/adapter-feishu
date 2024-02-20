25a92287dedf4786d7adfd36c2c7328579e15069
StarHeartHunt <starheart233@gmail.com>
2024-02-20 10:29:42 +0800

    :art: improve tests structure

--- tests/test_feishu_connection.py
+++ tests/test_feishu_connection.py
@@ -8,26 +8,25 @@ from nonebot.drivers import URL
 import nonebot
 from nonebot.adapters.feishu import Adapter
 
-with (Path(__file__).parent / "bots.json").open("r") as f:
+with (Path(__file__).parent.joinpath("data", "bots.json")).open("r") as f:
     feishu_bots = json.load(f)
 
-test_bot = feishu_bots[0]
-bot_id = test_bot["app_id"]
+bot_id = feishu_bots[0]["app_id"]
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("endpoints", [f"/feishu/{bot_id}"])
 async def test_http(app: App, endpoints: str, server_url: URL):
-    with (Path(__file__).parent / "events.json").open("r") as f:
-        test_events = json.load(f)
+    with (Path(__file__).parent.joinpath("data", "payloads.json")).open("r") as f:
+        test_payloads = json.load(f)
 
     adapter = nonebot.get_adapter(Adapter)
-    adapter.feishu_config.feishu_api_base = str(server_url)
+    adapter.feishu_config.feishu_api_base = str(server_url)  # type: ignore
 
     async with app.test_server() as ctx:
         client = ctx.get_client()
-        event = test_events[0]
-        resp = await client.post(endpoints, json=event)
+        payload = test_payloads[0]
+        resp = await client.post(endpoints, json=payload)
         assert resp.status_code == 200
         bots = nonebot.get_bots()
         assert bot_id in bots
---
+++ tests/test_feishu_event.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+from typing import Any, Dict, List
+
+import pytest
+
+from nonebot.adapters.feishu import Adapter
+
+
+@pytest.mark.asyncio
+async def test_event():
+    with (Path(__file__).parent.joinpath("data", "events.json")).open("r") as f:
+        test_events: List[Dict[str, Any]] = json.load(f)
+
+    for event_data in test_events:
+        model_name: str = event_data.pop("_model", None)
+        event = Adapter.json_to_event(event_data)
+        assert model_name == event.__class__.__name__
