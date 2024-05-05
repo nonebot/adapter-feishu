6d144140ba423a99498ae60ff55093e01f848f82
StarHeartHunt <starheart233@gmail.com>
2024-05-05 10:52:54 +0800

    :loud_sound: add content to output

--- nonebot/adapters/feishu/adapter.py
+++ nonebot/adapters/feishu/adapter.py
@@ -187,7 +187,8 @@ class Adapter(BaseAdapter):
 
             raise NetworkError(
                 f"HTTP request received unexpected "
-                f"status code: {response.status_code}"
+                f"status code: {response.status_code}, "
+                f"response: {response.content}"
             )
 
         except FeishuAdapterException:
